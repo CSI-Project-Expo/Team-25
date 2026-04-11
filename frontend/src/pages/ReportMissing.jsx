43eb382130c4f02cee01468f7120422612664264
neilmalvalli-cyber <neil.malvalli@gmail.com>
2026-04-11 12:15:51 +0530

    final

--- frontend/src/pages/ReportMissing.jsx
+++ frontend/src/pages/ReportMissing.jsx
@@ -70,7 +70,7 @@ export default function ReportMissing(){
           className="bg-white p-8 rounded-xl shadow">
 
           <h1 className="text-2xl font-bold mb-6">
-            Report Missing Person
+            Report Missing Person (NEW)
           </h1>
 
           <input value={name}
