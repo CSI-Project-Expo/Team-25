eceae3f63be642eb9a1ed8a2157d6f5931059ee1
neilmalvalli-cyber <neil.malvalli@gmail.com>
2026-04-11 11:13:30 +0530

    fixed routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
 
 import Navbar from "./components/Navbar"
-import PageWrapper from "./components/PageWrapper"
 
 import Home from "./pages/Home"
 import ReportMissing from "./pages/ReportMissing"
@@ -12,23 +10,19 @@ import Admin from "./pages/Admin"
 import CaseDetails from "./pages/CaseDetails"
 
 export default function App() {
-
   return (
     <HashRouter>
       <Navbar />
 
-      <AnimatePresence mode="wait">
-        <Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report-missing" element={<ReportMissing />} />
+        <Route path="/report-sighting" element={<ReportSighting />} />
+        <Route path="/map" element={<LiveMap />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/case/:id" element={<CaseDetails />} />
+      </Routes>
 
-          <Route path="/" element={<PageWrapper><Home/></PageWrapper>} />
-          <Route path="/report-missing" element={<PageWrapper><ReportMissing/></PageWrapper>} />
-          <Route path="/report-sighting" element={<PageWrapper><ReportSighting/></PageWrapper>} />
-          <Route path="/map" element={<PageWrapper><LiveMap/></PageWrapper>} />
-          <Route path="/admin" element={<PageWrapper><Admin/></PageWrapper>} />
-          <Route path="/case/:id" element={<PageWrapper><CaseDetails/></PageWrapper>} />
-
-        </Routes>
-      </AnimatePresence>
     </HashRouter>
   )
 }
